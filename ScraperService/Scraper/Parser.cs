da454f2f7cf75b89d0f999b2e78533940f5f171e
Gafret Kelp <d.lemeshko@xlab-game.com>
2023-11-27 23:32:56 +0300

    feat: make parser pull data from websites

--- ScraperService/Scraper/Parser.cs
+++ ScraperService/Scraper/Parser.cs
@@ -2,46 +2,59 @@
 using Npgsql;
 using RecipeFetcherService.DbManagement.DataClasses;
 using RecipeFetcherService.DbManagement.Interfaces;
+using RecipeFetcherService.Scraper.Abstract;
 using RecipeFetcherService.Scraper.Interfaces;
 using RecipeFetcherService.Scraper.PatternDeserialization;
 
 namespace RecipeFetcherService.Scraper;
 
-public class Parser
+public class Parser : ParserBase
 {
     // TODO: refactor
-    private IRecipeRepository<Recipe, Ingredient> _recipeRepo;
-    private IIngredientRepository<Ingredient, Recipe> _ingredientRepo;
-    private IPatternReader<WebsitePatternRecord> _reader;
-    
-    public Parser(
-        IRecipeRepository<Recipe, Ingredient> recipeRepo, 
-        IIngredientRepository<Ingredient, Recipe> ingredientRepo, 
-        IPatternReader<WebsitePatternRecord> reader
-        )
-    {
-        _recipeRepo = recipeRepo;
-        _ingredientRepo = ingredientRepo;
-        _reader = reader;
-    }
 
-    public IEnumerable<WebsitePatternRecord> GetPatternsForRecipes(string pathToPatterns)
+    public Parser(
+        IRecipeRepository<Recipe, Ingredient> recipeRepo,
+        IIngredientRepository<Ingredient, Recipe> ingredientRepo,
+        IPatternReader<WebsitePatternRecord> reader
+    ) : base(recipeRepo, ingredientRepo, reader){}
+
+    
+    // helpers
+    private IEnumerable<WebsitePatternRecord> GetPatternsForRecipes(string pathToPatterns)
     {
         var patterns = _reader.ReadPatterns(pathToPatterns);
         return patterns;
     }
 
-    public async Task<HtmlDocument> GetPage(string url)
+    private async Task<HtmlDocument> GetPage(string url)
     {
         return await WebCrawler.GetHtmlDocument(url);
     }
 
-    public List<HtmlNode>? GetPageAttrs(HtmlDocument page, ITagPattern pattern)
+    private List<HtmlNode>? GetPageAttrs(HtmlDocument page, ITagPattern pattern)
     {
         return WebCrawler.FindTags(page, pattern);
     }
+    private string GetText(HtmlNode titleElement)
+    {
+        return titleElement.InnerText.Trim();
+    }
 
-    public List<Ingredient> ToIngredients(List<HtmlNode> ingredientElements)
+    private string GetElementAttr(HtmlNode element, string attrName)
+    {
+        return element.GetAttributeValue(attrName, null);
+    }
+
+    private (List<HtmlNode>, HtmlNode, HtmlNode) GetRecipeAttrs(HtmlDocument recipePage, WebsitePatternRecord pattern)
+    {
+        var ingredientElements = GetPageAttrs(recipePage, pattern.IngredientsPattern);
+        var titleElement = GetPageAttrs(recipePage, pattern.TitlePattern)?.FirstOrDefault();
+        var imageUrlElement = GetPageAttrs(recipePage, pattern.ImagePattern)?.FirstOrDefault();
+
+        return (ingredientElements, titleElement, imageUrlElement);
+    }
+
+    private List<Ingredient> MapToIngredients(List<HtmlNode> ingredientElements)
     {
         List<Ingredient> ingredients = new List<Ingredient>();
         foreach (var ingredientElement in ingredientElements)
@@ -50,12 +63,16 @@ public class Parser
             {
                 Name = ingredientElement.InnerText,
             };
+            ingredients.Add(ingredient);
         }
 
         return ingredients;
     }
 
-    public IEnumerable<int> AddIngredientsToDb(IEnumerable<Ingredient> ingredients)
+    
+    
+    // db work
+    private IEnumerable<int> AddIngredientsToDb(IEnumerable<Ingredient> ingredients)
     {
         HashSet<int> ingredientIds = new HashSet<int>();
         int id = 0;
@@ -71,6 +88,10 @@ public class Parser
                 {
                     id = _ingredientRepo.GetByName(ingredient.Name).IngredientId;
                 }
+                else
+                {
+                    continue;
+                }
             }
 
             ingredientIds.Add(id);
@@ -79,59 +100,60 @@ public class Parser
         return ingredientIds;
     }
 
-    public int AddRecipeToDb(Recipe recipe)
+    private int AddRecipeToDb(Recipe recipe)
     {
-        int id = 0;
+        int id;
         try
         {
             id = _recipeRepo.Insert(recipe);
         }
-        catch
+        catch (PostgresException exception)
         {
-            id = _recipeRepo.GetByTitle(recipe.Title).RecipeId;
+            if (exception.ConstraintName == "disallow_duplicate_urls" ||
+                exception.ConstraintName == "recipes_title_key")
+            {
+                id = _recipeRepo.GetByTitle(recipe.Title).RecipeId;
+            }
+            else throw;
         }
 
         return id;
     }
-
-    private string GetTitle(HtmlNode titleElement)
-    {
-        return titleElement.InnerText.Trim();
-    }
-
-    private string GetImageUrl(HtmlNode imageElement)
-    {
-        return imageElement.GetAttributeValue("src", null);
-    }
-
-    private (List<HtmlNode>, HtmlNode, HtmlNode) GetRecipeAttrs(HtmlDocument recipePage, WebsitePatternRecord pattern)
-    {
-        var ingredientElements = GetPageAttrs(recipePage, pattern.IngredientsPattern);
-        var titleElement = GetPageAttrs(recipePage, pattern.TitlePattern).FirstOrDefault();
-        var imageUrlElement = GetPageAttrs(recipePage, pattern.ImagePattern).FirstOrDefault();
-
-        return (ingredientElements, titleElement, imageUrlElement);
-    }
-
-    private void AddEntryToDb(Recipe recipe, IEnumerable<Ingredient> ingredients)
+    
+    private void AddFullEntryToDb(Recipe recipe, IEnumerable<Ingredient> ingredients)
     {
         int recipeId = AddRecipeToDb(recipe);
         IEnumerable<int> ingredientIds = AddIngredientsToDb(ingredients);
 
-        _recipeRepo.InsertReferencesForIngredients(recipeId, ingredientIds);
+        try
+        {
+            _recipeRepo.InsertReferencesForIngredients(recipeId, ingredientIds);
+        }
+        catch(PostgresException exc)
+        {
+            if (exc.ConstraintName == "recipe_ingredients_pkey")
+                return;
+            else
+                throw;
+        }
     }
     
+    
+    
+    // main methods
     private async Task ParseRecipe(string recipeUrl, WebsitePatternRecord pattern)
     {
-        if (recipeUrl is null) return;
-                
         HtmlDocument recipePage = await GetPage(recipeUrl);
-        var (ingredientElements, titleElement, imageUrlElement) = GetRecipeAttrs(recipePage, pattern);
+        var (ingredientElements, 
+            titleElement, 
+            imageUrlElement) = GetRecipeAttrs(recipePage, pattern);
         
 
-        List<Ingredient> ingredients = ToIngredients(ingredientElements);
-        string title = GetTitle(titleElement);
-        string imageUrl = GetImageUrl(imageUrlElement);
+        List<Ingredient> ingredients = MapToIngredients(ingredientElements);
+        string title = GetText(titleElement);
+        string imageUrl = null;
+        if(imageUrlElement is not null) 
+            imageUrl = GetElementAttr(imageUrlElement, "src");
 
         Recipe recipe = new Recipe()
         {
@@ -140,30 +162,47 @@ public class Parser
             Url = recipeUrl,
         };
 
-        AddEntryToDb(recipe, ingredients);
+        AddFullEntryToDb(recipe, ingredients);
     }
     
     private async Task ParseWebsite(WebsitePatternRecord pattern)
     {
-        HtmlDocument mainPage = await GetPage(pattern.Website);
-        List<HtmlNode>? recipeLinks = GetPageAttrs(mainPage, pattern.LinkPattern);
-            
-        if (recipeLinks is null) return;
+        HtmlDocument pageWithCategories = await GetPage(pattern.Website);
+        List<HtmlNode>? categories = GetPageAttrs(pageWithCategories, pattern.RecipeCategoryPattern);
+        if(categories is null)
+            return;
 
+        foreach (var category in categories)
+        {
+            string categoryPageUrl = category.GetAttributeValue("href", null);
+            if(categoryPageUrl is null) continue;
+            await ParseCategory(categoryPageUrl, pattern);
+        }
+    }
+    
+    private async Task ParseCategory(string categoryPageUrl, WebsitePatternRecord pattern)
+    {
+        HtmlDocument categoryPage = await GetPage(categoryPageUrl);
+        List<HtmlNode>? recipeLinks = GetPageAttrs(categoryPage, pattern.LinkPattern);
+            
+        if (recipeLinks is null) 
+            return;
+        
         foreach (var recipeLink in recipeLinks)
         {
             string recipeUrl = recipeLink.GetAttributeValue("href", null);
-
-            ParseRecipe(recipeUrl, pattern);
+            if (recipeUrl is null || recipeUrl.Contains("/article/") || recipeUrl.Contains("/gallery/")) continue;
+            await ParseRecipe(recipeUrl, pattern);
         }
     }
 
-    public async Task Parse(string pathToPatterns)
+    public override async Task Parse(string pathToPatterns)
     {
         var patterns = GetPatternsForRecipes(pathToPatterns);
+        
         foreach (var pattern in patterns)
         {
-            ParseWebsite(pattern);
+            await ParseWebsite(pattern);
         }
     }
 }
