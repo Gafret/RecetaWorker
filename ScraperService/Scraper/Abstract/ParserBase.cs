b1f13c65256815d0ff0dca136ad8e90f77b54304
Gafret Kelp <d.lemeshko@xlab-game.com>
2023-11-28 18:49:16 +0300

    refactor: add base class for parser

---
+++ ScraperService/Scraper/Abstract/ParserBase.cs
@@ -0,0 +1,25 @@
+﻿using RecipeFetcherService.DbManagement.DataClasses;
+using RecipeFetcherService.DbManagement.Interfaces;
+using RecipeFetcherService.Scraper.Interfaces;
+using RecipeFetcherService.Scraper.PatternDeserialization;
+
+namespace RecipeFetcherService.Scraper.Abstract;
+
+public abstract class ParserBase
+{
+    protected IRecipeRepository<Recipe, Ingredient> _recipeRepo;
+    protected IIngredientRepository<Ingredient, Recipe> _ingredientRepo;
+    protected IPatternReader<WebsitePatternRecord> _reader; 
+    
+    protected ParserBase(
+        IRecipeRepository<Recipe, Ingredient> recipeRepo, 
+        IIngredientRepository<Ingredient, Recipe> ingredientRepo, 
+        IPatternReader<WebsitePatternRecord> reader
+    )
+    {
+        _recipeRepo = recipeRepo;
+        _ingredientRepo = ingredientRepo;
+        _reader = reader;
+    }
+    public abstract Task Parse(string pathToPatterns);
+}
