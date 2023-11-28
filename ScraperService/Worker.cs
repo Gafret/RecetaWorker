180848e30793ab3320511b5fd1a5968c00e5eb08
Gafret Kelp <d.lemeshko@xlab-game.com>
2023-11-28 18:42:36 +0300

    feat: add parsing element to Worker.cs

--- ScraperService/Worker.cs
+++ ScraperService/Worker.cs
@@ -1,30 +1,37 @@
 using System.Data;
+using Dapper;
 using Microsoft.Extensions.Options;
 using Npgsql;
-using RecipeFetcherService.DbManagement;
 using RecipeFetcherService.DbManagement.DataClasses;
 using RecipeFetcherService.DbManagement.Interfaces;
 using RecipeFetcherService.DbManagement.Repositories;
+using RecipeFetcherService.Scraper;
+using RecipeFetcherService.Scraper.Abstract;
+using RecipeFetcherService.Scraper.PatternDeserialization;
 
 namespace RecipeFetcherService;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
-    private readonly IRepository<Recipe> _recipeRepository;
-    private readonly IRepository<Ingredient> _ingredientRepository;
+    private readonly IRecipeRepository<Recipe, Ingredient> _recipeRepository;
+    private readonly IIngredientRepository<Ingredient, Recipe> _ingredientRepository;
     
     public Worker(ILogger<Worker> logger, IOptions<DbOptions> dbOptions)
     {
-        IDbConnection connection = new NpgsqlConnection(dbOptions.Value.Connection);
+        IDbConnection connection = new NpgsqlConnection(dbOptions.Value.PostgreSqlTest);
         
         _logger = logger;
         _recipeRepository = new RecipeRepository(connection);
         _ingredientRepository = new IngredientRepository(connection);
+        
+        DefaultTypeMap.MatchNamesWithUnderscores = true;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        ParserBase parser = new Parser(_recipeRepository, _ingredientRepository, new TagPatternReader());
+        await parser.Parse("./tag_pattern.json");
         while (!stoppingToken.IsCancellationRequested)
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
